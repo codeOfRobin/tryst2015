d65b8b1c854df34ed3529b69ce53ae1f76e3f021
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-01-18 00:56:18 +0530

    finished details View.

--- tryst2015/detailsView.h
+++ tryst2015/detailsView.h
@@ -9,9 +9,9 @@
 #import <UIKit/UIKit.h>
 #import <Parse/Parse.h>
 #define WIDTH_MARGIN 12
-#define HEIGHT_MARGIN_TOP 127
+#define HEIGHT_MARGIN_TOP 95
 #define HEIGHT_MARGIN_BOTTOM 60
 @interface detailsView : UIView
-@property (nonatomic,strong) UIScrollView *mainView;
+@property (nonatomic,strong) UIView *mainView;
 @property (nonatomic,strong) PFObject *data;
 @end
--- tryst2015/detailsView.m
+++ tryst2015/detailsView.m
@@ -28,7 +28,7 @@
         [view setFrame:self.frame];
         [self addSubview:view];
         
-        self.mainView=[[UIScrollView alloc]initWithFrame:CGRectMake(WIDTH_MARGIN, HEIGHT_MARGIN_TOP, CGRectGetWidth(self.frame)-2*WIDTH_MARGIN, CGRectGetHeight(self.frame)-HEIGHT_MARGIN_TOP-HEIGHT_MARGIN_BOTTOM)];
+        self.mainView=[[UIView alloc]initWithFrame:CGRectMake(WIDTH_MARGIN, HEIGHT_MARGIN_TOP, CGRectGetWidth(self.frame)-2*WIDTH_MARGIN, CGRectGetHeight(self.frame)-HEIGHT_MARGIN_TOP-HEIGHT_MARGIN_BOTTOM)];
         [self addSubview:self.mainView];
         
         [self.mainView.layer setCornerRadius:8.0f];
@@ -50,52 +50,68 @@
         [self.mainView addSubview:headingBlurView];
         
         
-        UILabel *heading=[[UILabel alloc]initWithFrame:CGRectMake(0, 0, CGRectGetWidth(headingBlurView.frame), CGRectGetHeight(headingBlurView.frame))];
-        [heading setText:@"kasjncdksa"];
+        UILabel *heading=[[UILabel alloc]initWithFrame:CGRectMake(10, 0, CGRectGetWidth(headingBlurView.frame), CGRectGetHeight(headingBlurView.frame))];
+        [heading setText:@"BIZ QUIZ"];
+        [heading setFont:[UIFont fontWithName:@"DIN" size:30]];
         [headingBlurView addSubview:heading];
         [heading setTextColor:[UIColor whiteColor]];
         
-        UILabel *StartTimeHeading=[[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(image.frame)+10, self.mainView.frame.size.width/2, 30)];
+        UILabel *StartTimeHeading=[[UILabel alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(image.frame)+10, self.mainView.frame.size.width/2, 30)];
         [StartTimeHeading setTextColor:[UIColor whiteColor]];
         [StartTimeHeading setText:@"START TIME"];
+        [StartTimeHeading setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:24]];
         [self.mainView addSubview:StartTimeHeading];
         
-        UILabel *endTimeHeading=[[UILabel alloc]initWithFrame:CGRectMake(self.mainView.frame.size.width/2, CGRectGetMaxY(image.frame)+10,  self.mainView.frame.size.width/2, 30)];
+        UILabel *endTimeHeading=[[UILabel alloc]initWithFrame:CGRectMake(10+self.mainView.frame.size.width/2, CGRectGetMaxY(image.frame)+10,  self.mainView.frame.size.width/2, 30)];
         [endTimeHeading setTextColor:[UIColor whiteColor]];
         [endTimeHeading setText:@"END TIME"];
+        [endTimeHeading setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:24]];
         [self.mainView addSubview:endTimeHeading];
         
-        UILabel *startTime=[[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(StartTimeHeading.frame),  self.mainView.frame.size.width/2, 30)];
+        UILabel *startTime=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(StartTimeHeading.frame),  self.mainView.frame.size.width/2, 30)];
         [startTime setTextColor:[UIColor whiteColor]];
         [startTime setText:@"adkfjkasdf"];
         [self.mainView addSubview:startTime];
         
-        UILabel *endTime=[[UILabel alloc]initWithFrame:CGRectMake( self.mainView.frame.size.width/2, CGRectGetMaxY(StartTimeHeading.frame), self.mainView.frame.size.width/2, 30)];
+        UILabel *endTime=[[UILabel alloc]initWithFrame:CGRectMake( 10+self.mainView.frame.size.width/2, CGRectGetMaxY(StartTimeHeading.frame), self.mainView.frame.size.width/2, 30)];
         [endTime setTextColor:[UIColor whiteColor]];
         [endTime setText:@"adkfjkasdf"];
         [self.mainView addSubview:endTime];
         
-        UILabel *venueHeading=[[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(endTime.frame), CGRectGetWidth(self.mainView.frame), 30)];
+        UIView *border0=[[UIView alloc]initWithFrame:CGRectMake(CGRectGetMinX(endTimeHeading.frame)-10, CGRectGetMaxY(image.frame), 1,70)];
+        [border0 setBackgroundColor:[UIColor whiteColor]];
+        [self.mainView addSubview:border0];
+        
+        UIView *border1=[[UIView alloc] initWithFrame:CGRectMake(0,CGRectGetMaxY(endTime.frame), CGRectGetWidth(self.mainView.frame), 1)];
+        [border1 setBackgroundColor:[UIColor whiteColor]];
+        [self.mainView addSubview:border1];
+        
+        UILabel *venueHeading=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(endTime.frame), CGRectGetWidth(self.mainView.frame), 30)];
         [venueHeading setText:@"VENUE"];
         [venueHeading setTextColor:[UIColor whiteColor]];
+        [venueHeading setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:24]];
         [self.mainView addSubview:venueHeading];
         
-        UILabel *venue=[[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(venueHeading.frame),CGRectGetWidth(self.mainView.frame), 30)];
+        UILabel *venue=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(venueHeading.frame),CGRectGetWidth(self.mainView.frame), 30)];
         [venue setTextColor:[UIColor whiteColor]];
         [venue setText:@"ghjkl"];
         [self.mainView addSubview:venue];
         
-        UILabel *contactHeading=[[UILabel alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(venue.frame), CGRectGetWidth(self.mainView.frame), 30)];
+        UIView *border2=[[UIView alloc] initWithFrame:CGRectMake(0,CGRectGetMaxY(venue.frame), CGRectGetWidth(self.mainView.frame), 1)];
+        [border2 setBackgroundColor:[UIColor whiteColor]];
+        [self.mainView addSubview:border2];
+        
+        UILabel *contactHeading=[[UILabel alloc]initWithFrame:CGRectMake(10, CGRectGetMaxY(venue.frame), CGRectGetWidth(self.mainView.frame), 30)];
         [contactHeading setTextColor:[UIColor whiteColor]];
         [contactHeading setText:@"CONTACT"];
+        [contactHeading setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:24]];
         [self.mainView addSubview:contactHeading];
         
-        UILabel *contact=[[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(contactHeading.frame), CGRectGetWidth(self.mainView.frame), 30)];
+        UILabel *contact=[[UILabel alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(contactHeading.frame), CGRectGetWidth(self.mainView.frame), 30)];
         [contact setTextColor:[UIColor whiteColor]];
         [contact setText:@"asdkcjnakscd"];
         [self.mainView addSubview:contact];
         
-        [self.mainView setContentSize:CGSizeMake(CGRectGetWidth(self.mainView.frame), 1000)];
 
         UIButton *done=[[UIButton alloc]initWithFrame:CGRectMake(CGRectGetWidth(self.mainView.frame)-100 , CGRectGetMinY(self.mainView.frame)-30, 100, 30)];
         [done setTitle:@"Done" forState:UIControlStateNormal];
